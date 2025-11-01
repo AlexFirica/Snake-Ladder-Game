8f0062c0248cfee882a8d2a41dcd590e5c282972
JishinKs <todordaniel91@gmail.com>
2025-11-01 21:00:57 +0200

    Al_doilea_commit

--- Snake Ladder Game/MainWindow.xaml.cs
+++ Snake Ladder Game/MainWindow.xaml.cs
@@ -70,11 +70,109 @@ public partial class MainWindow : Window
         playerImage.ImageSource =  new BitmapImage(new Uri("pack://application:,,,/images/player.gif"));
         opponentImage.ImageSource =  new BitmapImage(new Uri("pack://application:,,,/images/opponent.gif"));
 
+        for (int i=0; i<100; i++)
+        {
+            images++;
+            
+            ImageBrush tileImages = new ImageBrush();
+            tileImages.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/" + images + ".jpg"));
+            Rectangle box = new Rectangle
+            {
+                Height = 60,
+                Width = 60,
+                Fill = tileImages,
+                Stroke = Brushes.Black,
+                StrokeThickness = 1,
+                
+            };
+            box.Name = "box" + i.ToString();
+            this.RegisterName(box.Name, box);
+            
+            Moves.Add(box);
 
+            if (a == 10)
+            {
+                topPos -= 60;
+                a = 30;
+            }
+            if (a==20)
+            {
+                topPos -= 60;
+                a = 0;
+            }
+            if(a>20)
+            {
+                a--;
+                Canvas.SetLeft(box, leftPos);
+                leftPos -= 60;
+            }
+            if(a<10)
+            {   a++; 
+                Canvas.SetLeft(box, leftPos);
+                leftPos += 60;
+                Canvas.SetLeft(box, leftPos);
+            }
+            Canvas.SetTop(box, topPos);
+
+            MyCanvas.Children.Add(box);
+
+        }
+
+        gameTimer.Tick += GameTimerEvent;
+        gameTimer.Interval = TimeSpan.FromSeconds(.2);
+
+        player = new Rectangle
+        {
+            Height = 30,
+            Width = 30,
+            Fill = playerImage,
+            StrokeThickness = 2
+        };
+
+        opponent = new Rectangle
+        {
+            Height = 30,
+            Width = 30,
+            Fill = opponentImage,
+            StrokeThickness = 2
+        };
+
+        MyCanvas.Children.Add(player);
+        MyCanvas.Children.Add(opponent);
+
+        MovePiece(player, "box" + 0);
     }
 
     private void RestartGame()
     {
+        i = -1;
+        j = -1;
+
+        MovePiece(player, "box" + 0);
+        MovePiece(opponent, "box" + 0);
+
+
+        position = 0;
+        currentPosition = 0;
+
+
+        opponentPosition = 0;
+        opponentCurrentPosition = 0;
+
+
+        playerOneRound = false;
+        playerTwoRound = false;
+
+
+        txtPlayer.Content = "You Rolled a" + position;
+        txtPlayerPosition.Content = "Player is @ 1";
+
+        txtOpponent.Content = "Opponent Rolled a " + opponentPosition;
+        txtOpponentPosition.Content = "Opponent is @ 1";
+
+        gameTimer.Stop();
+
+
 
     }
 
@@ -86,7 +184,18 @@ public partial class MainWindow : Window
 
     private void MovePiece(Rectangle player, string posName )
     {
+        foreach (Rectangle rectangle in Moves)
+        {
+            if (rectangle.Name == posName)
+            {
+                landingRec = rectangle;
+            }
+        }
 
+
+        Canvas.SetLeft(player, Canvas.GetLeft(landingRec) + player.Width / 2);
+
+        Canvas.SetTop(player, Canvas.GetTop(landingRec) + player.Height / 2);
     }
 
 
